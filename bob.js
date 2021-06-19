11ffba0dca4a1bbabbfc2e7f1e19978524953032
ISHITA-SHAW <83875590+ISHITA-SHAW@users.noreply.github.com>
2021-06-19 22:02:31 +0530

    Add files via upload

---
+++ bob.js
@@ -0,0 +1,20 @@
+class bob {
+    constructor(x,y){
+        var options = {
+            isStatic: false,
+            restitution:1,
+            density:7.8,
+        }
+        this.bodies = Bodies.circle(x,y,25,options);
+        World.add = (world,this.bodies)
+
+        console.log(this.body);
+    }
+    display(){
+        push();
+        ellipseMode(RADIUS);
+        fill(254,0,255);
+        ellipse(this.body.position.x,this.body.position.y,25,25);
+        pop();
+    }
+}
---
+++ roof.js
@@ -0,0 +1,20 @@
+class roof {
+    constructor(x,y,width,height){
+       this.body = bodies.rectangle(x,y,width,height,{isStatic : true})
+       this.width = width;
+       this.height = height;
+       World.add = (world,this.bodies)
+
+        }
+        
+        
+        
+    
+    display(){
+        push();
+        rectMode(CENTER);
+        fill(128,128,128);
+        rect(this.body.position.x,this.body.position.y,this.width,this.height);
+        pop();
+    }
+}
